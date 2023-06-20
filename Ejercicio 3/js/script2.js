39bd181d363bc8ec106b2903fa377fa3beea17be
Mauricio <m.millavil.escobar@gmail.com>
2023-06-20 02:36:17 -0400

    Ejercicio 3 JS

---
+++ Ejercicio 3/js/script2.js
@@ -0,0 +1,5 @@
+let amigos = ["Pedro", "Maria", "Joan", "Pili"];
+
+const misAmigos = ["Maria"];
+const misAmigos2 = ["Joan"];
+console.log(`Mis amigos son ${misAmigos} y ${misAmigos2}`);
---
+++ Ejercicio 3/js/script4.js
@@ -0,0 +1,7 @@
+const numeros = [2, 4, 5, 1, 2]
+
+function sumaLista () {
+    console.log(2 + 4 + 5 + 1 + 2)
+}
+
+sumaLista();
